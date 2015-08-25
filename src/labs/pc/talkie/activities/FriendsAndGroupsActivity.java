9962dee2dceb436d39efa55af5eee9a24f1cdb12
Prashant Chaturvedi <pc.iiitmk@gmail.com>
2015-08-25 22:11:05 +0530

    Changed the location of FriendsAndGroupActivity to dedicated .activity package

--- src/labs/pc/talkie/activities/FriendsAndGroupsActivity.java
+++ src/labs/pc/talkie/activities/FriendsAndGroupsActivity.java
@@ -1,22 +1,21 @@
-package labs.pc.talkie;
+package labs.pc.talkie.activities;
 
 import java.util.Locale;
 
-import android.app.Activity;
+import labs.pc.talkie.R;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class FriendsAndGroupsActivity extends Activity implements
 		ActionBar.TabListener {
